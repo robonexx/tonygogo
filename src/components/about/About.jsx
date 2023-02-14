881d4fb452a99147d3ac1b6901817f48d7395926
robonexx <robertwagar@gmail.com>
2023-02-14 09:33:24 +0100

    about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import IMGFANCY from '../../assets/images/tony_fancy.png'
+import IMGJUDGE from '../../assets/images/tony_judge.png'
 
 // styles
 import './about.scss'
@@ -7,13 +8,19 @@ import './about.scss'
 const About = () => {
   return (
       <div className='about'>
-          <img src={IMGFANCY} alt="tony in costume" />
+          <img className="img_judge" src={IMGJUDGE} alt="tony judge solo" />
+          <h2>ABOUT</h2>
           <p>
           Dancer and Choreographer Tony Go Go, co-founding member of the original Go Go Brothers, has preserved and developed the Go Go legacy since 1971 - the very beginning of sychronised Locking.
-<br/>
-Originally from Watts, California, Tony Go Go started doing a crazy dance called Campbellocking, with his friends from High School. The three of them went to perform on  Soul Train and then formed a group called the Go Go Brothers. While dancing with his  partners (James “ OG Skeeter Rabbit” Higgins, Edwin “Buddy GoGo”  Lombard, and Kevin YoYo ) they formed another group by the name of Creative Generation - boy did they spread the love and the funk! Partners in that group were Arnetta Johnson, Lorna Dune, Greg Cambellock Jr, Jimmy Scooby-Doo Foster and Tony’s fellow GoGo brothers ... But most of you remember Tony GoGo as a member of the Original Lockers with Don Campbell, Fluky Luke, Mr. Penguine, Shabba-Doo, and Slim The Robot.
+              <br />
+              <br/>
+              Originally from Watts, California, Tony Go Go started learning dance called Campbellocking, with his friends from High School (James “ OG Skeeter Rabbit” Higgins, Edwin “Buddy GoGo”  Lombard, and Kevin YoYo ). They went to perform on  Soul Train and then formed a group called the Go Go Brothers.
+              <br />
+              <br />
+              While dancing with his  partners (James “ OG Skeeter Rabbit” Higgins, Edwin “Buddy GoGo”  Lombard, and Kevin YoYo ) they formed another group by the name of Creative Generation - formed by The Watts Writers Workshop to develop talent around in Watts, Compton adn LA! Creative generation was the first unisex locking group. <br />
+              Partners in that group were Arnetta Johnson, Lorna Dune, Greg Cambellock Jr, Jimmy Scooby-Doo Foster and Tony’s fellow GoGo brothers ... But most of you remember Tony GoGo as a member of the Original Lockers with Don Campbell, Fluky Luke, Mr. Penguine, Shabba-Doo, and Slim The Robot.
           </p>
-          
+          <img src={IMGFANCY} alt="tony in costume" />
  <p>
 Since leaving the Original Lockers group in the early 1980s, Tony has taught and developed not only the original art of Locking in Japan but he recognised, created and developed a style that has by now become unique to the Asian style of Locking, sometimes refered to as ‘Lock Fusion.' Tony Go Go is now considered the innovator of the Japanese style of Locking.
 <br/>

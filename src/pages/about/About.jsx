16456677dc2cb946eadbd0348ab0a0395a88d47c
robonexx <robertwagar@gmail.com>
2023-02-14 13:58:20 +0100

    animation to about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {motion} from 'framer-motion'
 import IMGFANCY from '../../assets/images/tony_fancy.png'
 import IMGJUDGE from '../../assets/images/tony_judge.png'
 
@@ -8,9 +9,21 @@ import './about.scss'
 const About = () => {
   return (
       <div className='about'>
-          <img className="img_judge" src={IMGJUDGE} alt="tony judge solo" />
-          <h2>ABOUT</h2>
-          <p>
+      <motion.img className="img_judge" src={IMGJUDGE} alt="tony judge solo"
+        initial={{scale: 0}}
+        animate={{scale: 1}}
+        transition={{duration: 0.6, delay: 0.8}}
+      />
+      <motion.h2
+        initial={{ y: 200, opacity: 0 }}
+        animate={{y: 0, opacity: 1}}
+        transition={{duration: 1, delay: 1}}      
+      >ABOUT</motion.h2>
+      <motion.p
+       initial={{opacity: 0 }}
+       animate={{opacity: 1}}
+       transition={{duration: 1.6, delay: 1.6}} 
+      >
           Dancer and Choreographer Tony Go Go, co-founding member of the original Go Go Brothers, has preserved and developed the Go Go legacy since 1971 - the very beginning of sychronised Locking.
               <br />
               <br/>
@@ -19,7 +32,7 @@ const About = () => {
               <br />
               While dancing with his  partners (James “ OG Skeeter Rabbit” Higgins, Edwin “Buddy GoGo”  Lombard, and Kevin YoYo ) they formed another group by the name of Creative Generation - formed by The Watts Writers Workshop to develop talent around in Watts, Compton adn LA! Creative generation was the first unisex locking group. <br />
               Partners in that group were Arnetta Johnson, Lorna Dune, Greg Cambellock Jr, Jimmy Scooby-Doo Foster and Tony’s fellow GoGo brothers ... But most of you remember Tony GoGo as a member of the Original Lockers with Don Campbell, Fluky Luke, Mr. Penguine, Shabba-Doo, and Slim The Robot.
-          </p>
+          </motion.p>
           <img src={IMGFANCY} alt="tony in costume" />
  <p>
 Since leaving the Original Lockers group in the early 1980s, Tony has taught and developed not only the original art of Locking in Japan but he recognised, created and developed a style that has by now become unique to the Asian style of Locking, sometimes refered to as ‘Lock Fusion.' Tony Go Go is now considered the innovator of the Japanese style of Locking.
